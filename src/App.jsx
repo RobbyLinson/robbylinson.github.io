499f7cf8277fb7369b910f574b64a770ff2d7db7
RobbyLinson <robbylinson@gmail.com>
2026-02-27 15:09:36 +0000

    Removed Dailies, unused

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import Dailies from './pages/Dailies';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/dailies" element={<Dailies />} />
     </Routes>
   );
 }
